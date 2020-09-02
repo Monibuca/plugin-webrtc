a96420c474781490eb7b525e4cef261e4b480acc
langhuihui <178529795@qq.com>
2020-09-02 08:23:28 +0800

    修改UI错误

--- ui/src/components/Player.vue
+++ ui/src/components/Player.vue
@@ -37,7 +37,7 @@ props:{
 },
     methods: {
         async play(streamPath) {
-            pc = new RTCPeerConnection({iceServers:["turn:"+this.PublicIP]});
+            pc = new RTCPeerConnection({iceServers:[{urls:"turn:"+this.PublicIP}]});
             pc.addTransceiver('video',{
               direction:'recvonly'
             })
